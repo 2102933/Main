ae3b7472a820d9ddaa37cd97f3a28992500fe337
netmaster81 <61751225+netmaster81@users.noreply.github.com>
2022-04-21 10:50:54 +0200

    updated version of manu page

--- javaScript/menu_page.js
+++ javaScript/menu_page.js
@@ -1,258 +1,67 @@
-function myFun12(){
-    document.getElementById('item12').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun12del(){
-    document.getElementById('item12').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun11(){
-    document.getElementById('item11').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun11del(){
-    document.getElementById('item11').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun10(){
-    document.getElementById('item10').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun10del(){
-    document.getElementById('item10').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun9(){
-    document.getElementById('item9').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun9del(){
-    document.getElementById('item9').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun8(){
-    document.getElementById('item8').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun8del(){
-    document.getElementById('item8').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun7(){
-    document.getElementById('item7').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun7del(){
-    document.getElementById('item7').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun6(){
-    document.getElementById('item6').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun6del(){
-    document.getElementById('item6').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun5(){
-    document.getElementById('item5').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun5del(){
-    document.getElementById('item5').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun4(){
-    document.getElementById('item4').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun4del(){
-    document.getElementById('item4').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun3(){
-    document.getElementById('item3').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun3del(){
-    document.getElementById('item3').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun2(){
-    document.getElementById('item2').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun2del(){
-    document.getElementById('item2').style.display = 'none';
-    if (x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun1(){
-    document.getElementById('item1').style.display = 'inline';
-    document.getElementById('test13').style.display = 'inline';
-}
-function myFun1del(){
-    document.getElementById('item1').style.display = 'none';
-    if(x1.style.display == 'none' && x2.style.display == 'none' && x3.style.display == 'none' && x4.style.display == 'none' && x5.style.display == 'none' && x6.style.display == 'none' && x7.style.display == 'none' && x8.style.display == 'none' && x9.style.display == 'none' && x10.style.display == 'none' && x11.style.display == 'none' && x12.style.display == 'none'){
-        document.getElementById('test13').style.display='none';
-    }
-}
-function myFun13()
-            {
-                alert('order submited');
-            }
-var counter12 = 0, counter11=0, counter10=0, counter9=0, counter8=0, counter7=0, counter6=0, counter5=0, counter4=0, counter3=0, counter2=0, counter1=0;
-var x1 = document.getElementById('item1');
-var x2 = document.getElementById('item2');
-var x3 = document.getElementById('item3');
-var x4 = document.getElementById('item4');
-var x5 = document.getElementById('item5');
-var x6 = document.getElementById('item6');
-var x7 = document.getElementById('item7');
-var x8 = document.getElementById('item8');
-var x9 = document.getElementById('item9');
-var x10 = document.getElementById('item10');
-var x11 = document.getElementById('item11');
-var x12 = document.getElementById('item12');
-function plus12(){
-    counter12+=1;
-    document.getElementById('counter12').innerHTML=counter12;
-}
-function minus12(){
-    if(counter12>0){
-        counter12--;
-    }
-    document.getElementById('counter12').innerHTML=counter12;
-}
-function plus11(){
-    counter11+=1;
-    document.getElementById('counter11').innerHTML=counter11;
-}
-function minus11(){
-    if(counter11>0){
-        counter11--;
-    }
-    document.getElementById('counter11').innerHTML=counter11;
-}
-function plus10(){
-    counter10+=1;
-    document.getElementById('counter10').innerHTML=counter10;
-}
-function minus10(){
-    if(counter10>0){
-        counter10--;
-    document.getElementById('counter10').innerHTML=counter10;
-    }
-}
+$(document).ready(function(){
+    var orderList=[];
+    $('.orderButton').click(function(){
+        let foodName = $(this).siblings("h4").html();
+        
+        
+        let foodPrice=$(this).siblings("span").html();
+        
+        let foodImage= $(this).closest(".card").find("img").attr('src');
+        
+        var pickUpFood= " <div class='addOrderFood'>"+
+        "<h6 style='color:white; margin-left: 15px;'>"+foodName+"</h6>"+
+        "<img src='"+foodImage+"' class='image-fluid' style='display:inline-block; width: 50px; height: 50px; margin-left: 2px;'>"+
+        "<button id='minus' style='display: inline-block; border: none; margin-left:10px;'>-</button>"+
+        "<p class='counter' style='display: inline-block;margin-left:10px;'> 1 </p>"+
+        "<button id='plus' style='display: inline-block; border: none;margin-left:10px;'>+</button>"+
+        "<button class='deleteOrder' style='display: inline-block; border: none; margin-left:10px; background-color: rgb(193, 58, 58); color:white'>"+
+        "<span class='material-icons'>delete</span></button></div>";
+        
+        if(!orderList.filter(v => v==foodName).toString()){
+            orderList.push(foodName);
+            $("#orderFood").append(pickUpFood);
+            $("#placeYourOrder").show();
+        }
+        
 
-function plus9(){
-    counter9+=1;
-    document.getElementById('counter9').innerHTML=counter9;
-}
-function minus9(){
-    if(counter9>0){
-        counter9--;
-    document.getElementById('counter9').innerHTML=counter9;
-    }
-}
-function plus8(){
-    counter8+=1;
-    document.getElementById('counter8').innerHTML=counter8;
-}
-function minus8(){
-    if(counter8>0){
-        counter8--;
-    document.getElementById('counter8').innerHTML=counter8;
-    }
-}
-function plus7(){
-    counter7+=1;
-    document.getElementById('counter7').innerHTML=counter7;
-}
-function minus7(){
-    if(counter7>0){
-        counter7--;
-    document.getElementById('counter7').innerHTML=counter7;
-    }
-}
-function plus6(){
-    counter6+=1;
-    document.getElementById('counter6').innerHTML=counter6;
-}
-function minus6(){
-    if(counter6>0){
-        counter6--;
-    document.getElementById('counter6').innerHTML=counter6;
-    }
-}
-function plus5(){
-    counter5+=1;
-    document.getElementById('counter5').innerHTML=counter5;
-}
-function minus5(){
-    if(counter5>0){
-        counter5--;
-    document.getElementById('counter5').innerHTML=counter5;
-    }
-}
-function plus4(){
-    counter4+=1;
-    document.getElementById('counter4').innerHTML=counter4;
-}
-function minus4(){
-    if(counter4>0){
-        counter4--;
-    document.getElementById('counter4').innerHTML=counter4;
-    }
-}
-function plus3(){
-    counter3+=1;
-    document.getElementById('counter3').innerHTML=counter3;
-}
-function minus3(){
-    if(counter3>0){
-        counter3--;
-    document.getElementById('counter3').innerHTML=counter3;
-    }
-}
-function plus2(){
-    counter2+=1;
-    document.getElementById('counter2').innerHTML=counter2;
-}
-function minus2(){
-    if(counter2>0){
-        counter2--;
-    document.getElementById('counter2').innerHTML=counter2;
-    }
-}
-function plus1(){
-    counter1+=1;
-    document.getElementById('counter1').innerHTML=counter1;
-}
-function minus1(){
-    if(counter1>0){
-        counter1--;
-    document.getElementById('counter1').innerHTML=counter1;
-    }
-}
+    });
+    
+    $('body').on('click', '.deleteOrder', function(){
+        
+        let foodName = $(this).siblings("h6").html();
+        orderList=orderList.filter(v => v!=foodName);
+        let test= $(this).parents('.addOrderFood').remove();
+        if(orderList.length == 0){
+            $("#placeYourOrder").hide();
+        }
+      
+    });
+
+    $('body').on('click', '#plus', function(){
+
+        let counter=parseInt($(this).siblings("p").html());
+        $(this).siblings("p").html((counter+1).toString());
+       
+     });
+
+     $('body').on('click', '#minus', function(){
+
+        
+        let counter=parseInt($(this).siblings("p").html());
+        if(counter-1==0){
+            let foodName = $(this).siblings("h6").html();
+            orderList=orderList.filter(v => v!=foodName);
+            $(this).parents('.addOrderFood').remove();
+            if(orderList.length == 0){
+                $("#placeYourOrder").hide();
+            }
+        }else{
+            $(this).siblings("p").html((counter-1).toString());
+        }
+        
+       
+     });
+   
+   
+
+});

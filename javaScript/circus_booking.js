22c3085e65d118d69fd39775f51c6bf749631917
Maha <mahaelrays@gmail.com>
2022-04-21 11:27:56 +0200

    editing for javascript file

--- javaScript/circus_booking.js
+++ javaScript/circus_booking.js
@@ -12,43 +12,14 @@ $(document).ready(
                 $("#dayYouChoose").text("");
                 $("#dayYouChoose").append(day + '_' + numDay);
             } else {
-                alert("Bad");
+                alert("No Circus Show in this day");
+                e.preventDefault();
+             window.location.replace("./circus_booking.html");
+
             }
             console.log(day);
         });
-        $("#check").click(function (e) {
-            function ValidateEmail(inputText) {
-                var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                if (inputText.value.match(mailformat)) {
-                    alert("Valid email address!");
-                    document.form1.text1.focus();
-                    return true;
-                } else {
-                    alert("You have entered an invalid email address!");
-                    document.form1.text1.focus();
-                    return false;
-                }
-            }
-            var elm = $("#inputPerson3").val();
-            var email = $("#inputEmail3").val();
-            var checkEmail = ValidateEmail(email);
-            if (elm < 1 ) {
-                alert("you should add person");
-                e.preventDefault();
-                window.location.replace("./circus_booking.html");
-
-            } else if( checkEmail == false){
-                e.preventDefault();
-                window.location.replace("./circus_booking.html");
-                
-            }
-
-
-
-
-
-
-        });
+       
 
     })
 )
